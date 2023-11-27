3b54c1fcbdf3f0d8bce9f47a48c7d5ebbe318a50
kevjng <graychrome@gmail.com>
2023-11-26 22:05:24 -0300

    Metadata Dynamica + Nombre de categoria en layout

--- app/tienda/[categoria]/page.js
+++ app/tienda/[categoria]/page.js
@@ -1,28 +1,28 @@
-'use client'
+
 
 import ProductCard from "../../components/ui/ProductCard.js"
 
-/* export const generateMetadata = async ({params,}) =>{
+export const generateMetadata = async ({params,}) =>{
     return {
         title: 'BeeWaxed - ' + params.categoria
     }
-} */
+}
 
 const Tienda = ({ params }) => {
-    /* console.log(params) */
+    console.log(params)
 
-    /* const { categoria } = params */
+    const { categoria } = params
 
   
 
     return (
         <div className="container my-5" >
 
-           {/*  <div className="my-4">
+            <div className="my-4">
                 <h1>
                     {categoria}
                 </h1>
-            </div> */}
+            </div>
              
             <ProductCard/>
             
