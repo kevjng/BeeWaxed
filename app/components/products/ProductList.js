391b88cea3807a44be70011b5c5ac5a9134edd9a
kevjng <graychrome@gmail.com>
2023-12-06 14:33:01 -0300

    detalle de producto rediseño

--- app/components/products/ProductList.js
+++ app/components/products/ProductList.js
@@ -1,6 +1,6 @@
 import ProductCard from "./ProductCard";
 
-import { collection, getDoc, getDocs, query, where, limit } from "firebase/firestore"
+import { collection, getDocs, query, where, limit } from "firebase/firestore"
 import { db } from "../../../firebase/config"
 
 const getProducts = async (categoria) => {
@@ -19,11 +19,16 @@ const getProducts = async (categoria) => {
 
 
 const ProductsList = async ({ categoria }) => {
+
     const items = await getProducts(categoria)
 
     return (
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 flex-wrap">
+
             {items.map(product => <ProductCard key={product.slug} item={product} />)}
+             
+            
         </div>
     );
 };
